34583c37bbc56fe397f7d2681392eea31ffb25e2
Gilbert Torchon <gilbert@noukod.com>
2018-11-14 15:21:47 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,9 +21,9 @@ ON subgenres.id=series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM series 
-INNER JOIN characters 
-ON series.id=characters.series_id 
+  "SELECT title FROM series
+INNER JOIN characters
+ON series.id=characters.series_id
 WHERE characters.species='human';"
 end
 
